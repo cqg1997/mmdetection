6d8326a580826933eb9e2a278bc0cafe335bc6a3
Polar Bear <xueyeeeh@gmail.com>
2020-09-07 14:30:01 +0800

    test

--- mmdet/models/necks/pbfpn.py
+++ mmdet/models/necks/pbfpn.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from mmcv.cnn import ConvModule, constant_init,build_conv_layer
+from mmcv.cnn import ConvModule, constant_init, build_conv_layer
 
 from mmdet.core import auto_fp16
 from ..builder import NECKS
@@ -83,86 +83,90 @@ class PBFPN(FPN):
         self.downsample_filter = nn.ModuleList()
         self.upsample_filter = nn.ModuleList()
         for i in range(self.start_level + 1, self.backbone_end_level):
-           # d_f = nn.Conv2d(
-           #     out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True,
-           #     groups=out_channels)
-           # u_f = nn.Conv2d(
-           #     out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True,
-           #     groups=out_channels)
-            u_f = build_conv_layer(
-                dict(type='DCN', deform_groups=1),
-                out_channels,
-                out_channels,
-                kernel_size=3,
-                stride=1,
-                padding=2,
-                dilation=2,
-                bias=True)
-            
-            # self.downsample_filter.append(d_f)
+            d_f = nn.Conv2d(
+                out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True,
+                groups=out_channels)
+            u_f = nn.Conv2d(
+                out_channels, out_channels, kernel_size=3, stride=1, padding=2, dilation=2, bias=True,
+                groups=out_channels)
+            #  u_f = build_conv_layer(
+            #      dict(type='DCN', deform_groups=1),
+            #      out_channels,
+            #      out_channels,
+            #      kernel_size=3,
+            #      stride=1,
+            #      padding=2,
+            #      dilation=2,
+            #      bias=True)
+
+            self.downsample_filter.append(d_f)
             self.upsample_filter.append(u_f)
 
-    def init_weights(self):
-        super().init_weights()
-        for _, filter in enumerate(self.upsample_filter):
-            constant_init(filter.conv_offset, 0)
-            
 
-    @auto_fp16()
-    def forward(self, inputs):
-        """Forward function."""
-        assert len(inputs) == len(self.in_channels)
+def init_weights(self):
+    super().init_weights()
+    for _, filter in enumerate(self.upsample_filter):
+        constant_init(filter, 0, 1)
+    for _, filter in enumerate(self.downsample_filter):
+        constant_init(filter, 0, 1)
 
-        # build laterals
-        laterals = [
-            lateral_conv(inputs[i + self.start_level])
-            for i, lateral_conv in enumerate(self.lateral_convs)
-        ]
 
-        # build top-down path
-        used_backbone_levels = len(laterals)
-        for i in range(used_backbone_levels - 1, 0, -1):
-            prev_shape = laterals[i - 1].shape[2:]
-            up_feat = F.interpolate(laterals[i], size=prev_shape, mode='nearest')
-            
-            laterals[i - 1] += self.upsample_filter[i-1](up_feat) 
+
+@auto_fp16()
+def forward(self, inputs):
+    """Forward function."""
+    assert len(inputs) == len(self.in_channels)
+
+    # build laterals
+    laterals = [
+        lateral_conv(inputs[i + self.start_level])
+        for i, lateral_conv in enumerate(self.lateral_convs)
+    ]
+
+    # build top-down path
+    used_backbone_levels = len(laterals)
+    for i in range(used_backbone_levels - 1, 0, -1):
+        prev_shape = laterals[i - 1].shape[2:]
+        up_feat = F.interpolate(laterals[i], size=prev_shape, mode='nearest')
+        switch = self.upsample_filter[i-1](up_feat)
+
+        laterals[i - 1] = switch*up_feat + (1-switch)*laterals[i - 1]
 
         # build outputs
-        # part 1: from original levels
-        inter_outs = [
-            self.fpn_convs[i](laterals[i]) for i in range(used_backbone_levels)
-        ]
+    # part 1: from original levels
+    inter_outs = [
+        self.fpn_convs[i](laterals[i]) for i in range(used_backbone_levels)
+    ]
 
-        # part 2: add bottom-up path
-        for i in range(0, used_backbone_levels - 1):
-            feat = self.downsample_convs[i](inter_outs[i])
-            #mask = self.downsample_filter[i](feat)
-            inter_outs[i + 1] += feat
-        
+    # part 2: add bottom-up path
+    for i in range(0, used_backbone_levels - 1):
+        down_feat = self.downsample_convs[i](inter_outs[i])
+        switch = self.downsample_filter[i](down_feat)
+        inter_outs[i + 1] = switch* down_feat + (1-switch) * inter_outs[i+1]
 
-        outs = [inter_outs[0]]
-        outs.extend([
-            self.PBFPN_convs[i - 1](inter_outs[i])
-            for i in range(1, used_backbone_levels)
-        ])
+    outs = [inter_outs[0]]
+    outs.extend([
+        self.PBFPN_convs[i - 1](inter_outs[i])
+        for i in range(1, used_backbone_levels)
+    ])
 
-        # part 3: add extra levels
-        if self.num_outs > len(outs):
-            # use max pool to get more levels on top of outputs
-            # (e.g., Faster R-CNN, Mask R-CNN)
-            if not self.add_extra_convs:
-                for i in range(self.num_outs - used_backbone_levels):
-                    outs.append(F.max_pool2d(outs[-1], 1, stride=2))
-            # add conv layers on top of original feature maps (RetinaNet)
+    # part 3: add extra levels
+    if self.num_outs > len(outs):
+        # use max pool to get more levels on top of outputs
+        # (e.g., Faster R-CNN, Mask R-CNN)
+        if not self.add_extra_convs:
+            for i in range(self.num_outs - used_backbone_levels):
+                outs.append(F.max_pool2d(outs[-1], 1, stride=2))
+        # add conv layers on top of original feature maps (RetinaNet)
+        else:
+            if self.extra_convs_on_inputs:
+                orig = inputs[self.backbone_end_level - 1]
+                outs.append(self.fpn_convs[used_backbone_levels](orig))
             else:
-                if self.extra_convs_on_inputs:
-                    orig = inputs[self.backbone_end_level - 1]
-                    outs.append(self.fpn_convs[used_backbone_levels](orig))
+                outs.append(self.fpn_convs[used_backbone_levels](outs[-1]))
+            for i in range(used_backbone_levels + 1, self.num_outs):
+                if self.relu_before_extra_convs:
+                    outs.append(self.fpn_convs[i](F.relu(outs[-1])))
                 else:
-                    outs.append(self.fpn_convs[used_backbone_levels](outs[-1]))
-                for i in range(used_backbone_levels + 1, self.num_outs):
-                    if self.relu_before_extra_convs:
-                        outs.append(self.fpn_convs[i](F.relu(outs[-1])))
-                    else:
-                        outs.append(self.fpn_convs[i](outs[-1]))
-        return tuple(outs)
+                    outs.append(self.fpn_convs[i](outs[-1]))
+    return tuple(outs)
